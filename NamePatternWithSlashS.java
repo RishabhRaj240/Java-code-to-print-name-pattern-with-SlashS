6e1d004db606291a4a4597c29654590b04ab6316
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-14 23:33:12 +0530

    Add files via upload

---
+++ NamePatternWithSlashS.java
@@ -0,0 +1,8 @@
+class NamePatternWithSlashS {
+    public static void main(String[] args) {
+         System.out.println("H");
+         System.out.println("\s\sA");
+         System.out.println("\s\s\s\sR");
+         System.out.println("\s\s\s\s\s\sI");
+    }
+}
